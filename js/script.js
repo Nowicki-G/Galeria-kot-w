0fbdc6f7bf98853bc65648c18941cda04a8750a1
Grzegorz Nowicki <agip240571@gmail.pl>
2024-09-03 12:38:29 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const THUMBNAILS = document.querySelectorAll('.thumbnail img');
+const POPUP = document.querySelector('.popup');
+const POPUP_CLOSE = document.querySelector('.popup__close');
+
+THUMBNAILS.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', () => {
+        POPUP.classList.remove('hidden');
+    });
+});
+
+POPUP_CLOSE.addEventListener('click', () => {
+    POPUP.classList.add('hidden');
+})
