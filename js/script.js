3fd2f6604106c0d252d60bb981575851353acf67
Grzegorz Nowicki <agip240571@gmail.pl>
2024-09-04 07:41:34 +0200

    addition event target in js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const THUMBNAILS = document.querySelectorAll('.thumbnail img');
-const POPUP = document.querySelector('.popup');
-const POPUP_CLOSE = document.querySelector('.popup__close');
+const THUMBNAILS = document.querySelectorAll(".thumbnail img");
+const POPUP = document.querySelector(".popup");
+const POPUP_CLOSE = document.querySelector(".popup__close");
+const POPUP__IMG = document.querySelector(".popup__img");
 
 THUMBNAILS.forEach((thumbnail) => {
-    thumbnail.addEventListener('click', () => {
-        POPUP.classList.remove('hidden');
+    thumbnail.addEventListener("click", (e) => {
+        POPUP.classList.remove("hidden");
+        POPUP__IMG.src = e.target.src;
     });
 });
 
-POPUP_CLOSE.addEventListener('click', () => {
-    POPUP.classList.add('hidden');
-})
+POPUP_CLOSE.addEventListener("click", () => {
+    POPUP.classList.add("hidden");
+});
+
+const myFunction = () => {};
