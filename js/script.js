768381791545812fe52789397fae31a6e140b85b
Grzegorz Nowicki <agip240571@gmail.pl>
2024-09-04 11:40:23 +0200

    Activation of the left and right buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
 const THUMBNAILS = document.querySelectorAll(".thumbnail img");
 const POPUP = document.querySelector(".popup");
 const POPUP_CLOSE = document.querySelector(".popup__close");
-const POPUP__IMG = document.querySelector(".popup__img");
+const POPUP_IMG = document.querySelector(".popup__img");
+const ARROW_LEFT = document.querySelector(".popup__arrow--left");
+const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
-THUMBNAILS.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", (e) => {
-        POPUP.classList.remove("hidden");
-        POPUP__IMG.src = e.target.src;
-    });
+let currentImgIndex;
+
+THUMBNAILS.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", (e) => {
+    POPUP.classList.remove("hidden");
+    POPUP_IMG.src = e.target.src;
+    currentImgIndex = index;
+  });
 });
 
 POPUP_CLOSE.addEventListener("click", () => {
-    POPUP.classList.add("hidden");
+  POPUP.classList.add("hidden");
 });
 
-const myFunction = () => {};
+ARROW_RIGHT.addEventListener("click", () => {
+  if (currentImgIndex === THUMBNAILS.length + 1) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex++;
+  }
+  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+});
+
+ARROW_LEFT.addEventListener("click", () => {
+  if (currentImgIndex === 0) {
+    currentImgIndex = THUMBNAILS.length - 1;
+  } else {
+    currentImgIndex--;
+  }
+  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight" / e.keyCode === 39) {
+  }
+});
