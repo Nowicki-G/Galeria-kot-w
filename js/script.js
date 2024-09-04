385758e46d7fca1504a0554b9eafe7c91cb1ea38
Grzegorz Nowicki <agip240571@gmail.pl>
2024-09-04 13:02:12 +0200

    restore js

--- js/script.js
+++ js/script.js
@@ -7,27 +7,6 @@ const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
 let currentImgIndex;
 
-<<<<<<< HEAD
-const showNextImg = () => {
-  if (currentImgIndex === THUMBNAILS.length + 1) {
-    currentImgIndex = 0;
-  } else {
-    currentImgIndex++;
-  }
-  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
-};
-
-const showPrevImg = () => {
-  if (currentImgIndex === 0) {
-    currentImgIndex = THUMBNAILS.length - 1;
-  } else {
-    currentImgIndex--;
-  }
-  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
-};
-
-=======
->>>>>>> fb2c189a6154e1d77becb41240d4418ad0da66a8
 THUMBNAILS.forEach((thumbnail, index) => {
   thumbnail.addEventListener("click", (e) => {
     POPUP.classList.remove("hidden");
@@ -40,18 +19,6 @@ POPUP_CLOSE.addEventListener("click", () => {
   POPUP.classList.add("hidden");
 });
 
-<<<<<<< HEAD
-ARROW_RIGHT.addEventListener("click", showNextImg);
-
-ARROW_LEFT.addEventListener("click", showPrevImg);
-
-document.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowRight" / e.keyCode === 39) {
-    showNextImg();
-  }
-  if(e.code === "ArrowLeft" / e.keycode === 37) {
-    showPrevImg();
-=======
 ARROW_RIGHT.addEventListener("click", () => {
   if (currentImgIndex === THUMBNAILS.length + 1) {
     currentImgIndex = 0;
@@ -72,6 +39,5 @@ ARROW_LEFT.addEventListener("click", () => {
 
 document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowRight" / e.keyCode === 39) {
->>>>>>> fb2c189a6154e1d77becb41240d4418ad0da66a8
   }
 });
